73974d34543d174f92d097731735b74a222f622d
fishy <140210758+the-fishy@users.noreply.github.com>
2024-02-08 22:03:13 -0800

    Update BP_Throwable_Attributes.cpp

--- BP_Throwable_Attributes.cpp
+++ BP_Throwable_Attributes.cpp
@@ -8,11 +8,7 @@
 // Sets default values for this component's properties
 UBP_Throwable_Attributes::UBP_Throwable_Attributes()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -20,18 +16,7 @@ UBP_Throwable_Attributes::UBP_Throwable_Attributes()
 void UBP_Throwable_Attributes::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
-}
-
-
-// Called every frame
-void UBP_Throwable_Attributes::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
 void UBP_Throwable_Attributes::MultiplyGrenadeRadius(float MultiplyBy)
